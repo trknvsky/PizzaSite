e6311e48e2b7af7e1af6c438fed58f8686d4b7c0
trknvsky <trknvsky@gmail.com>
2019-12-10 19:22:15 +0200

    add unittests

--- order/tests.py
+++ order/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class UrlsTest(TestCase):	
+    def test_order_page(self):
+        response = self.client.get("/order/")
+        self.assertEqual(response.status_code, 200)
